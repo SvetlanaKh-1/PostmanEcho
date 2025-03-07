a46311c687cb3879a22d9ca0fb39e4eade83d5eb
Svetlana Khlystova <khlystova_sv@bk.ru>
2025-03-08 00:04:35 +0300

    Test succes

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/ssytwxbqfflhsvl2?svg=true)](https://ci.appveyor.com/project/SvetlanaKh-1/postmanecho)
--- src/test/java/ru/netology/postman/PostmanEchoTest.java
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -19,7 +19,7 @@ class PostmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello"))
+                .body("data", equalTo("Hello!"))
         ;
     }
 }
